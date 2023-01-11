2a824a3b739a7066918beb8a7e89835d42a8530e
Dafeigy <1070642765@qq.com>
2023-01-11 23:57:22 +0800

    Update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 import lxml
 import glob
 import tarfile
+import time
 from bs4 import BeautifulSoup
 
 url_base = 'https://arxiv.org/e-print/'
@@ -87,48 +88,51 @@ def get_index_per_page(search_url):
     return [each.get('href')[8:] for each in results]
 
 if __name__ == '__main__':
-    url = 'https://arxiv.org/list/cs/2001'
-    show_per_page = 50 # This should be larger than 5
-    target_num = get_paper_num(url)
-    pages = target_num//show_per_page
-    data = pd.DataFrame()
-
-    for i in range(pages):
-        temp = pd.DataFrame()
-        formula_list = []
-        search_url = 'https://arxiv.org/list/cs/2001?skip={}&show={}'.format(i * show_per_page,show_per_page)
-        download_list = get_index_per_page(search_url)
-        temp['paper_id'] = download_list
-        for each in download_list:
-            try:
-                tar_file = download_tar(each, DOWNLOAD_DIR)
-            except:
-                print("\033[1;31;40mCDownload Failed.\033[0m")
-                formula_list.append("Download Failed.")
-                continue
-            try:
-                untar_file = un_tar(tar_file)
-            except:
-                print("\033[1;31;40mCould not Unzip .tar file.\033[0m")
-                formula_list.append("Unzip Failed.")
-                continue
-            try:
-                texs = find_texs(untar_file)
-            except:
-                print("\033[1;31;40mCounld not load texes.\033[0m")
-                formula_list.append("Counld not load texes")
-                continue
-            try:
-                content = str(parse_texs(texs))
-            except:
-                print("\033[1;31;40mParsing Failed.\033[0m")
-                formula_list.append("Parsing Failed.")
-                continue
-            formula_list.append(content)
-            # clean_files(tar_file, untar_file)
-        temp['formula_json'] = formula_list
-        data = pd.concat([data, temp], axis=0)
-        data.to_csv("Outputs/2001.csv",index=False)
+    idlist = [i for i in range(2001,2013)]
+    for ID in idlist:
+        url = 'https://arxiv.org/list/cs/{}'.format(ID)
+        show_per_page = 50 # This should be larger than 5
+        target_num = get_paper_num(url)
+        pages = target_num//show_per_page
+        data = pd.DataFrame()
+        # data = pd.read_csv('Outputs/{}.csv'.format(ID), encoding='utf-8')
+        for i in range(pages):
+            temp = pd.DataFrame()
+            formula_list = []
+            search_url = 'https://arxiv.org/list/cs/{}?skip={}&show={}'.format(ID, i * show_per_page,show_per_page)
+            download_list = get_index_per_page(search_url)
+            temp['paper_id'] = download_list
+            for each in download_list:
+                try:
+                    tar_file = download_tar(each, DOWNLOAD_DIR)
+                except:
+                    print("\033[1;31;40mCDownload Failed.\033[0m")
+                    formula_list.append("Download Failed.")
+                    continue
+                try:
+                    untar_file = un_tar(tar_file)
+                except:
+                    print("\033[1;31;40mCould not Unzip .tar file.\033[0m")
+                    formula_list.append("Unzip Failed.")
+                    continue
+                try:
+                    texs = find_texs(untar_file)
+                except:
+                    print("\033[1;31;40mCounld not load texes.\033[0m")
+                    formula_list.append("Counld not load texes")
+                    continue
+                try:
+                    content = str(parse_texs(texs))
+                except:
+                    print("\033[1;31;40mParsing Failed.\033[0m")
+                    formula_list.append("Parsing Failed.")
+                    continue
+                formula_list.append(content)
+                clean_files(tar_file, untar_file)
+            temp['formula_json'] = formula_list
+            data = pd.concat([data, temp], axis=0)
+            data.to_csv("Outputs/{}.csv".format(ID),index=False)
+        print("Task {} Finished.".format(ID))
     
 
         
